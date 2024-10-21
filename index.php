e0370eb1f3ecde13333820d02c37c8eda61e9319
RobertoNobleMaestro <10007851.joan23@fje.edu>
2024-10-21 20:37:57 +0200

    verificaciones javascript

--- index.php
+++ index.php
@@ -1,34 +1,37 @@
-<?php
-session_abort();
-session_start();
-?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Pagina principal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="scripts/verificaciones.js"></script>
+    <title>Página principal</title>
+    <style>
+        .invalido-select {
+            border: 3px solid #f5c6cb; 
+        }
+    </style>
 </head>
 <body>
-    <form action="lados.php" method="post" class="form-index">
+    <form action="lados.php" method="post" class="form-index" onsubmit="return index();">
         <div class="align-center">
-            <h1>Geometria</h1>
+            <h1>Geometría</h1>
         </div>
         <br>
         <label for="producto" class="form-label">Selecciona el tipo de forma</label>
-            <select class="form-select" id="forma" name="forma" required>
-                <option selected disabled>Elige una forma</option>
-                <option value="Esfera">Esfera</option>
-                <option value="Triangulo">Triangulo</option>
-                <option value="Cuadrado">Cuadrado</option>
-                <option value="Rectangulo">Rectangulo</option>
-            </select>
-            <br>
-            <br>    
-            <div class="align-center">
-                <input class="boton" type="submit" value="Enviar">
-            </div>
+        <select class="form-select" id="forma" name="forma" onblur="index()" onchange="index()">
+            <option value="" selected disabled>Elige una forma</option> 
+            <option value="Esfera">Esfera</option>
+            <option value="Triangulo">Triángulo</option>
+            <option value="Cuadrado">Cuadrado</option>
+            <option value="Rectangulo">Rectángulo</option>
+        </select>
+        <br>
+        <p id="error" style="color: red;"></p>
+        <br>
+        <div class="align-center">
+            <input class="boton" type="submit" value="Enviar">
+        </div>
     </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-</html>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--- lados.php
+++ lados.php
@@ -2,10 +2,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <script src="scripts/"></script>
+    <script src="scripts/verificaciones.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
+<style>
+.invalido-select {
+    border: 3px solid #f5c6cb; 
+}
+</style>
 <body>
 <?php
 session_start();
@@ -21,38 +26,50 @@ if (isset($_POST['forma'])) {
 // Formulario y cálculo según la forma seleccionada
 if ($_SESSION['forma'] === "Triangulo") {
     // Formulario para un triángulo
-    echo '<form action="resultado.php" method="post" class="form-index">
-        <h1>' . $_SESSION["forma"] . '</h1>
-        <label for="lado1" class="form-label">Base: </label>
-        <input type="number" name="Base"  class="form-control"><br><br>
-        <label for="lado2" class="form-label">Altura: </label>
-        <input type="number" name="Altura"  class="form-control"><br><br>
-        <input type="submit" name="calcular" value="Calcular" class="boton">
+    echo '<form action="resultado.php" method="post" class="form-index" onsubmit="return triangulo()">
+    <h1>' . $_SESSION["forma"] . '</h1>
+    <label for="lado1" class="form-label">Base: </label>
+    <input type="text" name="lado1" id="lado1" class="form-control" onblur="triangulo()" onchange="triangulo()"><br>
+    <p id="errorBase" style="color: red;"></p> 
+    <label for="lado2" class="form-label">Altura: </label>
+    <input type="text" name="lado2" id="lado2" class="form-control" onblur="triangulo()" onchange="triangulo()"><br>
+    <p id="errorAltura" style="color: red;"></p> 
+    <br>
+    <input type="submit" name="calcular" value="Calcular" class="boton">
     </form>';
 } elseif ($_SESSION['forma'] === "Esfera") {
     // Formulario para una esfera
-    echo '<form action="resultado.php" method="post" class="form-index">
-            <h1>' . $_SESSION["forma"] . '</h1>
-            <label for="radio" class="form-label">Radio de la esfera:</label>
-            <input type="number" name="radio"  class="form-control" required><br><br>
-            <input type="submit" name="calcular" value="Calcular"class="boton">
-          </form>';
+    echo '<form action="resultado.php" method="post" class="form-index" onsubmit="return redonda()">
+        <h1>Esfera</h1>
+        <label for="radio" class="form-label">Radio de la esfera:</label>
+        <input type="text" name="radio" id="radio" class="form-control" onblur="redonda()" onchange="redonda()"><br>
+        <p id="error" style="color: red;"></p>
+        <br>
+        <input type="submit" name="calcular" value="Calcular" class="boton">
+        </form>';
 
 } elseif ($_SESSION['forma'] === "Rectangulo") {
     // Formulario para un rectángulo
-    echo '<form action="resultado.php" method="post" class="form-index">
+    echo '<form action="resultado.php" method="post" class="form-index" onsubmit="return rectangulo()">
         <h1>' . $_SESSION["forma"] . '</h1>
         <label for="lado1" class="form-label">Lado 1:</label>
-        <input type="number" name="lado1"  class="form-control"><br><br>
+        <input type="text" name="lado1" id="lado1" class="form-control" onblur="rectangulo()" onchange="rectangulo()"><br>
+        <p id="error1" style="color: red;"></p> 
+        <br>
         <label for="lado2" class="form-label">Lado 2:</label>
-        <input type="number" name="lado2"  class="form-control"><br><br>
+        <input type="text" name="lado2" id="lado2" class="form-control"  onblur="rectangulo()" onchange="rectangulo()"><br>
+        <p id="error2" style="color: red;"></p> 
+        <br>
         <input type="submit" name="calcular" value="Calcular" class="boton">
         </form>';
+
 } elseif ($_SESSION['forma'] === "Cuadrado"){
-    echo '<form action="resultado.php" method="post" class="form-index">
+    echo '<form action="resultado.php" method="post" class="form-index" onsubmit="return  cuadrado()">
         <h1>' . $_SESSION["forma"] . '</h1>
         <label for="lado1" class="form-label">Lado 1:</label>
-        <input type="number" name="lado1"  class="form-control"><br><br>
+        <input type="text" name="lado1" id="lado1" class="form-control" onblur="cuadrado()" onchange="cuadrado()"><br>
+        <p id="error" style="color: red;"></p>
+        <br>
         <input type="submit" name="calcular" value="Calcular" class="boton">
         </form>';
 } else {
